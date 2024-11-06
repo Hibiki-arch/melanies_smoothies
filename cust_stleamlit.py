275f227ce9a48408e7fbc1ceb5a999fb38616f7a
Hibiki-arch <mesaki.hibiki.5044@members.co.jp>
2024-11-06 14:25:19 +0900

    Update cust_stleamlit.py

--- cust_stleamlit.py
+++ cust_stleamlit.py
@@ -78,6 +78,6 @@ if time_to_insert:
     st.success('注文したよん!', icon="✅");
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon");
-st.text(fruityvice_response.json())
+st.write(fruityvice_response.json())
 
 # fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)

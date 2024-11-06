222589ac422b2a2ec5800add300ebea1b0722937
Hibiki-arch <mesaki.hibiki.5044@members.co.jp>
2024-11-06 13:10:18 +0900

    Update cust_stleamlit.py

--- cust_stleamlit.py
+++ cust_stleamlit.py
@@ -3,7 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 from snowflake.snowpark.context import get_active_session
 
-cnx = st.connection("snowflake)
+cnx = st.connection("snowflake")
 session = cnx.session()
 
 # Write directly to the app

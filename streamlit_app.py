34a43337d0c0b83db84ce3f48e79f10353cf4b32
Hibiki-arch <mesaki.hibiki.5044@members.co.jp>
2024-11-06 12:16:52 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.write(
 )
 
 # ↓テーブル表示
-session = get_active_session();
+session = st.session_state;
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
